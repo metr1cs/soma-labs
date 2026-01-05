fbe742565ab6178029f51af1cc0bc763180f1214
Kez 92 <kez92@MacBook-Air-Kez.local>
2026-01-06 01:37:31 +0300

    fix photo

--- js/data/cases-data.js
+++ js/data/cases-data.js
@@ -5,12 +5,12 @@ export const casesData = [
         title: 'Coswick.market',
         tag: 'E-commerce',
         category: 'sites', //
-        imageSrc: './image/cases/Frame 1312317285.webp',
+        imageSrc: '/image/cases/Frame 1312317285.webp',
 
         slides: [
-            { src: './image/cases/Снимок экрана 2026-01-05 в 21.38.05.png', caption: 'Главная страница' },
-            { src: './image/cases/Снимок экрана 2026-01-05 в 21.39.14.png', caption: 'Карточка товара' },
-            { src: './image/cases/Снимок экрана 2026-01-05 в 21.40.20.png', caption: 'Админ панель' }
+            { src: '/image/cases/Снимок экрана 2026-01-05 в 21.38.05.png', caption: 'Главная страница' },
+            { src: '/image/cases/Снимок экрана 2026-01-05 в 21.39.14.png', caption: 'Карточка товара' },
+            { src: '/image/cases/Снимок экрана 2026-01-05 в 21.40.20.png', caption: 'Админ панель' }
         ]
     },
     {
@@ -18,12 +18,12 @@ export const casesData = [
         title: 'Юридическая помощь в Нижневартовске',
         tag: 'E-commerce',
         category: 'sites', //
-        imageSrc: './image/cases/Frame 1312317278.webp',
+        imageSrc: '/image/cases/Frame 1312317278.webp',
 
         slides: [
-            { src: './image/cases/Снимок экрана 2026-01-05 в 21.44.50.png', caption: 'Главная страница' },
-            { src: './image/cases/Снимок экрана 2026-01-05 в 21.38.05.png', caption: 'Карточка товара' },
-            { src: './image/cases/Снимок экрана 2026-01-05 в 21.40.20.png', caption: 'Админ панель' }
+            { src: '/image/cases/Снимок экрана 2026-01-05 в 21.44.50.png', caption: 'Главная страница' },
+            { src: '/image/cases/Снимок экрана 2026-01-05 в 21.38.05.png', caption: 'Карточка товара' },
+            { src: '/image/cases/Снимок экрана 2026-01-05 в 21.40.20.png', caption: 'Админ панель' }
         ]
     },
     {
@@ -31,12 +31,12 @@ export const casesData = [
         title: 'Азбука-Пола',
         tag: 'E-commerce',
         category: 'sites', //
-        imageSrc: './image/cases/Снимок экрана 2026-01-06 в 01.07.50.png',
+        imageSrc: '/image/cases/Снимок экрана 2026-01-06 в 01.07.50.png',
 
         slides: [
-            { src: './image/cases/Снимок экрана 2026-01-05 в 21.44.04.png', caption: 'Главная страница' },
-            { src: './image/cases/Снимок экрана 2026-01-05 в 21.45.40.png', caption: 'Карточка товара' },
-            { src: './image/cases/Снимок экрана 2026-01-05 в 21.46.35.png', caption: 'Админ панель' }
+            { src: '/image/cases/Снимок экрана 2026-01-05 в 21.44.04.png', caption: 'Главная страница' },
+            { src: '/image/cases/Снимок экрана 2026-01-05 в 21.45.40.png', caption: 'Карточка товара' },
+            { src: '/image/cases/Снимок экрана 2026-01-05 в 21.46.35.png', caption: 'Админ панель' }
         ]
     },
     {
@@ -44,12 +44,12 @@ export const casesData = [
         title: 'Центр коррекции и развития в Тюмени',
         tag: 'E-commerce',
         category: 'sites', //
-        imageSrc: './image/cases/Frame 1312317280.webp',
+        imageSrc: '/image/cases/Frame 1312317280.webp',
 
         slides: [
-            { src: './image/cases/Снимок экрана 2026-01-05 в 21.44.04.png', caption: 'Главная страница' },
-            { src: './image/cases/Снимок экрана 2026-01-05 в 21.45.40.png', caption: 'Карточка товара' },
-            { src: './image/cases/Снимок экрана 2026-01-05 в 21.46.35.png', caption: 'Админ панель' }
+            { src: '/image/cases/Снимок экрана 2026-01-05 в 21.44.04.png', caption: 'Главная страница' },
+            { src: '/image/cases/Снимок экрана 2026-01-05 в 21.45.40.png', caption: 'Карточка товара' },
+            { src: '/image/cases/Снимок экрана 2026-01-05 в 21.46.35.png', caption: 'Админ панель' }
         ]
     },
     {
@@ -57,19 +57,19 @@ export const casesData = [
         title: 'Клиника №1',
         tag: 'E-commerce',
         category: 'sites', //
-        imageSrc: './image/cases/Frame 1312317284.webp',
+        imageSrc: '/image/cases/Frame 1312317284.webp',
 
         slides: [
-            { src: './image/cases/Снимок экрана 2026-01-06 в 01.00.21.png', caption: 'Главная страница' },
-            { src: './image/cases/Снимок экрана 2026-01-06 в 01.01.24.png', caption: 'Карточка товара' },
-            { src: './image/cases/Снимок экрана 2026-01-06 в 01.02.16.png', caption: 'Админ панель' }
+            { src: '/image/cases/Снимок экрана 2026-01-06 в 01.00.21.png', caption: 'Главная страница' },
+            { src: '/image/cases/Снимок экрана 2026-01-06 в 01.01.24.png', caption: 'Карточка товара' },
+            { src: '/image/cases/Снимок экрана 2026-01-06 в 01.02.16.png', caption: 'Админ панель' }
         ]
     },
     {
         id: 'lumina-tech',
         title: 'Lumina Tech Logo',
         tag: 'identity',
-        imageSrc: './image/cases/Frame 1312317284 (1).webp',
+        imageSrc: '/image/cases/Frame 1312317284 (1).webp',
         slides: [
             { src: 'https://images.unsplash.com/photo-1586717791821-3f44a563dc4c?auto=format&fit=crop&q=80&w=1200', caption: 'Логотип на светлом фоне' },
             { src: 'https://images.unsplash.com/photo-1557804506-669a67965ba0?auto=format&fit=crop&q=80&w=1200', caption: 'Визитные карточки' }
@@ -80,7 +80,7 @@ export const casesData = [
         id: 'lumina-tech',
         title: 'Lumina Tech Logo',
         tag: 'identity',
-        imageSrc: './image/cases/Frame 1312317283.webp',
+        imageSrc: '/image/cases/Frame 1312317283.webp',
         slides: [
             { src: 'https://images.unsplash.com/photo-1586717791821-3f44a563dc4c?auto=format&fit=crop&q=80&w=1200', caption: 'Логотип на светлом фоне' },
             { src: 'https://images.unsplash.com/photo-1557804506-669a67965ba0?auto=format&fit=crop&q=80&w=1200', caption: 'Визитные карточки' }
@@ -90,7 +90,7 @@ export const casesData = [
         id: 'lumina-tech',
         title: 'Lumina Tech Logo',
         tag: 'identity',
-        imageSrc: './image/cases/Frame 1312317282.webp',
+        imageSrc: '/image/cases/Frame 1312317282.webp',
         slides: [
             { src: 'https://images.unsplash.com/photo-1586717791821-3f44a563dc4c?auto=format&fit=crop&q=80&w=1200', caption: 'Логотип на светлом фоне' },
             { src: 'https://images.unsplash.com/photo-1557804506-669a67965ba0?auto=format&fit=crop&q=80&w=1200', caption: 'Визитные карточки' }
@@ -100,7 +100,7 @@ export const casesData = [
         id: 'lumina-tech',
         title: 'Lumina Tech Logo',
         tag: 'identity',
-        imageSrc: './image/cases/Frame 1312317281.webp',
+        imageSrc: '/image/cases/Frame 1312317281.webp',
         slides: [
             { src: 'https://images.unsplash.com/photo-1586717791821-3f44a563dc4c?auto=format&fit=crop&q=80&w=1200', caption: 'Логотип на светлом фоне' },
             { src: 'https://images.unsplash.com/photo-1557804506-669a67965ba0?auto=format&fit=crop&q=80&w=1200', caption: 'Визитные карточки' }
